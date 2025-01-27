201812277e415a672ef86a6624c765ad76bd31aa
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2025-01-27 17:04:24 +0700

    first commit

--- main.js
+++ main.js
@@ -6,19 +6,18 @@ var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicURL =
+  "https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a";
 var bgMusicControls = true; // Show UI music control
-
-
 
 // ===================== start =======================
 // animation start after 1000 miliseconds
 setTimeout(init, 1000);
 
-var odrag = document.getElementById('drag-container');
-var ospin = document.getElementById('spin-container');
-var aImg = ospin.getElementsByTagName('img');
-var aVid = ospin.getElementsByTagName('video');
+var odrag = document.getElementById("drag-container");
+var ospin = document.getElementById("spin-container");
+var aImg = ospin.getElementsByTagName("img");
+var aVid = ospin.getElementsByTagName("video");
 var aEle = [...aImg, ...aVid]; // combine 2 arrays
 
 // Size of images
@@ -26,13 +25,18 @@ ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
 
 // Size of ground - depend on radius
-var ground = document.getElementById('ground');
+var ground = document.getElementById("ground");
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
 
 function init(delayTime) {
   for (var i = 0; i < aEle.length; i++) {
-    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
+    aEle[i].style.transform =
+      "rotateY(" +
+      i * (360 / aEle.length) +
+      "deg) translateZ(" +
+      radius +
+      "px)";
     aEle[i].style.transition = "transform 1s";
     aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
   }
@@ -40,32 +44,36 @@ function init(delayTime) {
 
 function applyTranform(obj) {
   // Constrain the angle of camera (between 0 and 180)
-  if(tY > 180) tY = 180;
-  if(tY < 0) tY = 0;
+  if (tY > 180) tY = 180;
+  if (tY < 0) tY = 0;
 
   // Apply the angle
-  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+  obj.style.transform = "rotateX(" + -tY + "deg) rotateY(" + tX + "deg)";
 }
 
 function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
+  ospin.style.animationPlayState = yes ? "running" : "paused";
 }
 
-var sX, sY, nX, nY, desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
+var sX,
+  sY,
+  nX,
+  nY,
+  desX = 0,
+  desY = 0,
+  tX = 0,
+  tY = 10;
 
 // auto spin
 if (autoRotate) {
-  var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
+  var animationName = rotateSpeed > 0 ? "spin" : "spinRevert";
   ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
 
 // add background music
 if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
+  document.getElementById("music-container").innerHTML += `
+<audio src="${bgMusicURL}" ${bgMusicControls ? "controls" : ""} autoplay loop>    
 <p>If you are reading this, it is because your browser does not support the audio element.</p>
 </audio>
 `;
@@ -76,12 +84,12 @@ document.onpointerdown = function (e) {
   clearInterval(odrag.timer);
   e = e || window.event;
   var sX = e.clientX,
-      sY = e.clientY;
+    sY = e.clientY;
 
   this.onpointermove = function (e) {
     e = e || window.event;
     var nX = e.clientX,
-        nY = e.clientY;
+      nY = e.clientY;
     desX = nX - sX;
     desY = nY - sY;
     tX += desX * 0.1;
@@ -110,30 +118,12 @@ document.onpointerdown = function (e) {
   return false;
 };
 
-document.onmousewheel = function(e) {
+document.onmousewheel = function (e) {
   e = e || window.event;
   var d = e.wheelDelta / 20 || -e.detail;
   radius += d;
   init(1);
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 var canvas = document.getElementById("canvas");
 
@@ -141,8 +131,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Initialize the GL context
-var gl = canvas.getContext('webgl');
-if(!gl){
+var gl = canvas.getContext("webgl");
+if (!gl) {
   console.error("Unable to initialize WebGL.");
 }
 
@@ -309,24 +299,23 @@ void main(){
 
 //************** Utility functions **************
 
-window.addEventListener('resize', onWindowResize, false);
+window.addEventListener("resize", onWindowResize, false);
 
-function onWindowResize(){
-  canvas.width  = window.innerWidth;
+function onWindowResize() {
+  canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-	gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.viewport(0, 0, canvas.width, canvas.height);
   gl.uniform1f(widthHandle, window.innerWidth);
   gl.uniform1f(heightHandle, window.innerHeight);
 }
 
-
 //Compile shader and combine with source
-function compileShader(shaderSource, shaderType){
+function compileShader(shaderSource, shaderType) {
   var shader = gl.createShader(shaderType);
   gl.shaderSource(shader, shaderSource);
   gl.compileShader(shader);
-  if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-  	throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
   }
   return shader;
 }
@@ -336,7 +325,7 @@ function compileShader(shaderSource, shaderType){
 function getAttribLocation(program, name) {
   var attributeLocation = gl.getAttribLocation(program, name);
   if (attributeLocation === -1) {
-  	throw 'Cannot find attribute ' + name + '.';
+    throw "Cannot find attribute " + name + ".";
   }
   return attributeLocation;
 }
@@ -344,7 +333,7 @@ function getAttribLocation(program, name) {
 function getUniformLocation(program, name) {
   var attributeLocation = gl.getUniformLocation(program, name);
   if (attributeLocation === -1) {
-  	throw 'Cannot find uniform ' + name + '.';
+    throw "Cannot find uniform " + name + ".";
   }
   return attributeLocation;
 }
@@ -363,12 +352,16 @@ gl.linkProgram(program);
 
 gl.useProgram(program);
 
-//Set up rectangle covering entire canvas 
+//Set up rectangle covering entire canvas
 var vertexData = new Float32Array([
-  -1.0,  1.0, 	// top left
-  -1.0, -1.0, 	// bottom left
-   1.0,  1.0, 	// top right
-   1.0, -1.0, 	// bottom right
+  -1.0,
+  1.0, // top left
+  -1.0,
+  -1.0, // bottom left
+  1.0,
+  1.0, // top right
+  1.0,
+  -1.0, // bottom right
 ]);
 
 //Create vertex buffer
@@ -377,21 +370,22 @@ gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
 
 // Layout of our data in the vertex buffer
-var positionHandle = getAttribLocation(program, 'position');
+var positionHandle = getAttribLocation(program, "position");
 
 gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(positionHandle,
-  2, 				// position is a vec2 (2 values per component)
+gl.vertexAttribPointer(
+  positionHandle,
+  2, // position is a vec2 (2 values per component)
   gl.FLOAT, // each component is a float
-  false, 		// don't normalize values
-  2 * 4, 		// two 4 byte float components per vertex (32 bit float is 4 bytes)
-  0 				// how many bytes inside the buffer to start from
-  );
+  false, // don't normalize values
+  2 * 4, // two 4 byte float components per vertex (32 bit float is 4 bytes)
+  0 // how many bytes inside the buffer to start from
+);
 
 //Set uniform handle
-var timeHandle = getUniformLocation(program, 'time');
-var widthHandle = getUniformLocation(program, 'width');
-var heightHandle = getUniformLocation(program, 'height');
+var timeHandle = getUniformLocation(program, "time");
+var widthHandle = getUniformLocation(program, "width");
+var heightHandle = getUniformLocation(program, "height");
 
 gl.uniform1f(widthHandle, window.innerWidth);
 gl.uniform1f(heightHandle, window.innerHeight);
@@ -399,14 +393,13 @@ gl.uniform1f(heightHandle, window.innerHeight);
 var lastFrame = Date.now();
 var thisFrame;
 
-function draw(){
-	
+function draw() {
   //Update time
-	thisFrame = Date.now();
-  time += (thisFrame - lastFrame)/1000;	
-	lastFrame = thisFrame;
+  thisFrame = Date.now();
+  time += (thisFrame - lastFrame) / 1000;
+  lastFrame = thisFrame;
 
-	//Send uniforms to program
+  //Send uniforms to program
   gl.uniform1f(timeHandle, time);
   //Draw a triangle strip connecting vertices 0-4
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
